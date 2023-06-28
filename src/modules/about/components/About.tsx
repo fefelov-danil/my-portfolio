81b60857fc0fa2b8c291ad8a0bd6850303943e9b
Daniil Fefelov <fefelov.danil@gmail.com>
2023-06-28 21:00:17 +0400

    feat: animation for about page

--- src/modules/about/components/About.tsx
+++ src/modules/about/components/About.tsx
@@ -1,83 +1,91 @@
+'use client'
 import styles from './About.module.scss'
-import me from '../../../assets/images/me.png'
-import lines1 from '../../../assets/images/background/lines1.png'
+import me from '@/public/images/me.png'
+import lines1 from '@/public/images/background/lines1.png'
+import { stringsForBg } from '@/modules/about/constants/bigStringForBg'
+import { StringBg } from '@/modules/about/components/stringBg/StringBg'
 import { motion } from 'framer-motion'
+import Image from 'next/image'
+import iconSet from '@/assets/icons/selection.json'
 import IcomoonReact from 'icomoon-react'
-import { skills } from '../constants/skills.ts'
-import { stringsForBg } from '../constants/bigStringForBg.ts'
-import { StringBg } from './stringBg/StringBg.tsx'
-import { animation } from '../../../constants/animation.ts'
-import iconSet from '../../../assets/icons/selection.json'
+import { skills } from '@/modules/about/constants/skills'
+import { animation } from '@/constants/animation'
 
 export const About = () => {
   return (
-    <motion.div
-      className={styles.about}
-      initial={'hidden'}
-      whileInView={'visible'}
-      viewport={{ once: true, amount: 0.5 }}
-    >
+    <div className={styles.about}>
       <div className={styles.info}>
-        <motion.div variants={animation.fadeRightAnimation} className={styles.linesBg}>
-          <img src={lines1} alt={''} />
+        <motion.div initial={'hidden'} whileInView={'visible'} viewport={{ once: true, amount: 0.1 }}>
+          <motion.div variants={animation.fadeRightAnimation} className={styles.linesBg}>
+            <Image src={lines1} alt={''} />
+          </motion.div>
         </motion.div>
         <div className={styles.text}>
-          <h1>
-            <span>Фефелов Даниил</span>
-            <br />
-            front-end developer
-          </h1>
-          <ul className={styles.skills}>
+          <motion.div initial={'hidden'} whileInView={'visible'} viewport={{ once: true, amount: 0.1 }}>
+            <motion.h1 variants={animation.fadeBottomAnimation}>
+              <span>Фефелов Даниил</span>
+              <br />
+              front-end developer
+            </motion.h1>
+          </motion.div>
+          <motion.ul
+            className={styles.skills}
+            initial={'hidden'}
+            whileInView={'visible'}
+            viewport={{ once: true, amount: 0.1 }}
+          >
             {skills.map((skill, index) => (
-              <li key={index} className={styles.skill}>
+              <motion.li
+                key={index}
+                className={styles.skill}
+                variants={animation.fadeBottomAnimation}
+                custom={index * 0.1 + 0.15}
+              >
                 <IcomoonReact iconSet={iconSet} color={'#fff'} icon={skill.icon} size={45} />
                 <br />
                 {skill.name}
-              </li>
+              </motion.li>
             ))}
-          </ul>
-          <p className={styles.subtitle}>Здравствуйте! </p>
-          <p>С начала 2022 года я занимаюсь разработкой приложений. Пишу на React js и Next js использую TypeScript.</p>
-          <p>
-            Имею опыт работы в команде в разных проектах, на каждом из которых многому научился. Команды были от 3х до 8
-            человек.
-          </p>
-          <p>
-            У меня большой опыт в верстке. С 2016 года занимаюсь разработкой сайтов. Сложно посчитать, но думаю сделал
-            более 200 сайтов.
-          </p>
-          <p>
-            К работе отношусь ответственно, вникаю в задачу для меня важен результат. Я рад когда вижу положительную
-            обратную связь.
-          </p>
-          <p>
-            К работе отношусь ответственно, вникаю в задачу для меня важен результат. Я рад когда вижу положительную
-            обратную связь.
-          </p>
-          <p>
-            К работе отношусь ответственно, вникаю в задачу для меня важен результат. Я рад когда вижу положительную
-            обратную связь.
-          </p>
-          <p>
-            К работе отношусь ответственно, вникаю в задачу для меня важен результат. Я рад когда вижу положительную
-            обратную связь.
-          </p>
-          <p>
-            К работе отношусь ответственно, вникаю в задачу для меня важен результат. Я рад когда вижу положительную
-            обратную связь.
-          </p>
+          </motion.ul>
+          <motion.div initial={'hidden'} whileInView={'visible'} viewport={{ once: true, amount: 0.1 }}>
+            <motion.p className={styles.subtitle} variants={animation.fadeBottomAnimation} custom={0.6}>
+              Здравствуйте!{' '}
+            </motion.p>
+            <motion.p variants={animation.fadeBottomAnimation} custom={0.7}>
+              С начала 2022 года я занимаюсь разработкой приложений. Пишу на React js и Next js использую TypeScript.
+            </motion.p>
+            <motion.p variants={animation.fadeBottomAnimation} custom={0.8}>
+              Имею опыт работы в команде в разных проектах, на каждом из которых многому научился. Команды были от 3х до
+              8 человек.
+            </motion.p>
+            <motion.p variants={animation.fadeBottomAnimation} custom={0.9}>
+              У меня большой опыт в верстке. С 2016 года занимаюсь разработкой сайтов. Сложно посчитать, но думаю сделал
+              более 200 сайтов.
+            </motion.p>
+            <motion.p variants={animation.fadeBottomAnimation} custom={1}>
+              К работе отношусь ответственно, вникаю в задачу, для меня важен результат. Я рад когда вижу положительную
+              обратную связь.
+            </motion.p>
+          </motion.div>
         </div>
-        <motion.div variants={animation.fadeRightAnimation} className={`${styles.linesBg} ${styles.linesBgBottom}`}>
-          <img src={lines1} alt={''} />
+        <motion.div
+          className={`${styles.linesBg} ${styles.linesBgBottom}`}
+          initial={'hidden'}
+          whileInView={'visible'}
+          viewport={{ once: true }}
+        >
+          <motion.div variants={animation.fadeRightAnimation}>
+            <Image src={lines1} alt={''} />
+          </motion.div>
         </motion.div>
       </div>
       <div className={styles.stringForBg}>
-        <p style={{ backgroundImage: `url(${me})` }}>
+        <p style={{ backgroundImage: `url(${me.src})` }}>
           {stringsForBg.map((str, i) => (
             <StringBg key={i} str={str} />
           ))}
         </p>
       </div>
-    </motion.div>
+    </div>
   )
 }
